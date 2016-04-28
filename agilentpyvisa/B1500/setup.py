1ecf276c0bc9d888e22828652962709d4e4b75a4
nada <nada@nada.com>
2016-04-28 11:25:54 +0200

    added feault smu_spgu seutp

--- agilentpyvisa/B1500/setup.py
+++ agilentpyvisa/B1500/setup.py
@@ -1,28 +1,6 @@
 from .enums import (ADCMode, Format, Filter, OutputMode, SeriesResistance, ADCTypes,)
 from collections import namedtuple
 
-class TestSetup(namedtuple('__TestSetup',
-                           ["channels",    # list of channel setups
-                            "highspeed_adc_number",
-                            # AV, default 1, 1-1013 = number or numberx initial
-                            # in auto
-                            "highspeed_adc_mode",          # AV, default ADCMode.auto
-                            "adc_modes",
-                            # AIT, [(type,ADCMode.auto for type in ADCTypes as
-                            # default
-                            "format",
-                            "output_mode",
-                            "filter"
-                            ])):
-
-    def __new__(cls, channels=[], highspeed_adc_number=1, highspeed_adc_mode=ADCMode.auto,
-        adc_modes=[], format=Format.ascii12_with_header_crl,
-        output_mode=OutputMode.with_primarysource, filter=Filter.enabled):
-        # add default values
-        return super(TestSetup, cls).__new__(cls, channels, highspeed_adc_number,
-            highspeed_adc_mode, adc_modes, format, output_mode, filter)
-
-
 
 class TestSetup(namedtuple('__TestSetup',
                            ["channels",    # list of channel setups
@@ -41,7 +19,7 @@ class TestSetup(namedtuple('__TestSetup',
 
     def __new__(cls, channels=[], highspeed_adc_number=1, highspeed_adc_mode=ADCMode.auto,
         adc_modes=[], format=Format.ascii12_with_header_crl,
-        output_mode=OutputMode.dataonly, filter=Filter.disabled,spgu_selector_setup=[]):
+        output_mode=OutputMode.dataonly, filter=Filter.enabled,spgu_selector_setup=[(0,0) for i in range(4)]):
         # add default values
         num_mes= len(set([c.measurement.mode for c in channels if c.measurement]))
         if any([x.spgu for x in channels]) and not spgu_selector_setup:
